3ba7b21bec67aa540c6dd9c90ed495a1417d6bbd
RariRajC <rari.raj@quizlet.com>
2026-07-30 22:58:09 +0530

    Use document hash for FAISS storage

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,12 +1,10 @@
 import os
 
 from langchain_community.vectorstores import FAISS
-
 from utils.embeddings import get_embeddings
 
 
 def create_vector_store(chunks):
-
     embeddings = get_embeddings()
 
     return FAISS.from_documents(
@@ -16,26 +14,15 @@ def create_vector_store(chunks):
 
 
 def save_vector_store(vector_store, document_hash):
+    folder = os.path.join("vector_store", document_hash)
 
-    folder = os.path.join(
-        "vector_store",
-        document_hash,
-    )
-
-    os.makedirs(
-        folder,
-        exist_ok=True,
-    )
+    os.makedirs(folder, exist_ok=True)
 
     vector_store.save_local(folder)
 
 
 def load_vector_store(document_hash):
-
-    folder = os.path.join(
-        "vector_store",
-        document_hash,
-    )
+    folder = os.path.join("vector_store", document_hash)
 
     embeddings = get_embeddings()
 
